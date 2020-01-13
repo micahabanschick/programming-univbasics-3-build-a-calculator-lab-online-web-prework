126f72b12eac8da98036ab8628e173d4a54a8df9
Micah Banschick <micha.ban@gmail.com>
2020-01-13 00:12:08 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  return sum 
 end
 
 def subtraction(num1, num2)
-
+  dif = num1 - num2
+  return dif 
 end
 
 def division(num1, num2)
-
+  quo = num1 / num2
+  return quo 
 end
 
 def multiplication(num1, num2)
-
+  pro = num1 * num2
+  return pro 
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  return mod 
 end
 
 def square_root(num)
-
+  return Math.sqrt(num) 
 end
